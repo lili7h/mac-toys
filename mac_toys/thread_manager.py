c19168e47b0834c54496fa780f7253c64d584334
Lilith <__lilith@discord>
2024-06-10 18:00:50 +1000

    extra refactors for cleanliness

---
+++ mac_toys/thread_manager.py
@@ -0,0 +1,110 @@
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+from threading import Thread
+
+
+class ThreadedActor(ABC):
+    _actor: Thread = None
+
+    @property
+    def actor(self) -> Thread:
+        return self._actor
+
+    @actor.setter
+    def actor(self, actor: Thread) -> None:
+        self._actor = actor
+
+    @abstractmethod
+    def start(self) -> None:
+        pass
+
+    @abstractmethod
+    def stop(self, *, timeout: float = 1.0) -> None:
+        pass
+
+    @abstractmethod
+    def force_stop(self) -> None:
+        pass
+
+
+class ActorState(Enum):
+    NOT_STARTED = auto()
+    RUNNING = auto()
+    STOPPED = auto()
+
+
+class Agent:
+    _last_added: dict[str, ActorState | ThreadedActor] = None
+    actors: dict[str, dict[str, ActorState | ThreadedActor]] = None
+
+    def add_agent(self, name: str, actor: ThreadedActor) -> None:
+        if name in self.actors:
+            _state = self.actors[name].get('state')
+            _actor = self.actors[name].get('actor')
+            if _state == ActorState.RUNNING:
+                _actor.stop()
+
+        self.actors[name] = {
+            'state': ActorState.NOT_STARTED,
+            'actor': actor
+        }
+        self._last_added = self.actors[name]
+
+    def start(self, actor_name: str = None) -> None:
+        if actor_name is None:
+            _state = self._last_added.get('state')
+            _actor = self._last_added.get('actor')
+            if _state != ActorState.NOT_STARTED:
+                raise ValueError(f"Last added actor is already started.")
+
+            _actor.start()
+            self._last_added['state'] = ActorState.RUNNING
+        else:
+            _actor_d = self.actors.get(actor_name)
+            if _actor_d:
+                _state = _actor_d.get('state')
+                _actor = _actor_d.get('actor')
+
+                if _state != ActorState.NOT_STARTED:
+                    raise ValueError(f"Actor '{actor_name}' is already started.")
+
+                _actor.start()
+                _actor_d['state'] = ActorState.RUNNING
+            else:
+                raise KeyError(f"Actor of name '{actor_name}' does not exist.")
+
+    def stop(self, actor_name: str) -> None:
+        _actor_d = self.actors.get(actor_name)
+        if _actor_d:
+            _state = _actor_d.get('state')
+            _actor = _actor_d.get('actor')
+
+            if _state != ActorState.RUNNING:
+                raise ValueError(f"Actor '{actor_name}' is already stopped/not started.")
+
+            _actor.stop()
+            _actor_d['state'] = ActorState.STOPPED
+        else:
+            raise KeyError(f"Actor of name '{actor_name}' does not exist.")
+
+    def start_all(self) -> None:
+        for thread_name in self.actors:
+            _state = self.actors[thread_name].get('state')
+            _actor = self.actors[thread_name].get('actor')
+            match _state:
+                case ActorState.NOT_STARTED:
+                    _actor.start()
+                    self.actors[thread_name]['state'] = ActorState.RUNNING
+                case _:
+                    pass
+
+    def stop_all(self) -> None:
+        for thread_name in self.actors:
+            _state = self.actors[thread_name].get('state')
+            _actor = self.actors[thread_name].get('actor')
+            match _state:
+                case ActorState.RUNNING:
+                    _actor.stop()
+                    self.actors[thread_name]['state'] = ActorState.STOPPED
+                case _:
+                    pass
--- mac_toys/vibrator.py
+++ mac_toys/vibrator.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 __version__ = "0.1.0a"
 
 import asyncio
-import os
 import queue
 import time
+from rich import print as rprint
 from asyncio import sleep, run, get_running_loop, new_event_loop, set_event_loop
 from threading import Lock, Thread, get_native_id
 from queue import Queue
@@ -16,6 +16,7 @@ from typing import Union, cast, Optional, Callable, Coroutine, Any
 from mac_toys.sse_listener import SSEListener, ChatEvent, KillEvent, Singleton
 from mac_toys.tracker import PlayerTracker, UpdateTypes
 from mac_toys.helpers import interpolate_value_bounded
+from mac_toys.thread_manager import ThreadedActor, Agent
 
 
 class ValueSlider:
@@ -262,23 +263,148 @@ class IntensityController:
     async def apply(self) -> None:
         self._applicator_func(self.combined_intensity)
 
+    def is_running(self) -> bool:
+        return self._running
+
+
+class AmbienceController(ThreadedActor):
+    # Ambient background vibration
+    last_change_time: float = None
+    computed_change_rate: float = None
+    computed_ambient_vibration: float = None
+    ambient_vibration: float = None
+    ambient_vibration_variance: float = None
+    ambient_vibration_change_rate: float = None
+    ambient_vibration_change_rate_variance: float = None
+    # application
+    intensity_controller: IntensityController = None
+    # control
+    _stop_flag: bool = None
+    _running: bool = None
+    # update lock
+    _param_lock: Lock = None
+
+    def __init__(self, intensity_controller: IntensityController) -> None:
+        self.current_vibration = 0.0
+        self.ambient_vibration = 0.10
+        self.ambient_vibration_variance = 0.05
+        self.ambient_vibration_change_rate = 3.0
+        self.ambient_vibration_change_rate_variance = 0.5
+        self.computed_change_rate = self.ambient_vibration_change_rate
+        self.computed_ambient_vibration = self.ambient_vibration
+        self.last_change_time = time.time()
+
+        self._param_lock = Lock()
+        self._stop_flag = False
+        self._running = False
+        self.intensity_controller = intensity_controller
+        self.actor = Thread(
+            target=self.settle_ambience,
+            name="Ambient Vibration Controller Thread",
+            daemon=True,
+        )
+
+    def start(self) -> None:
+        self.actor.start()
+        self._running = True
+
+    def stop(self, *, timeout: float = 1.0) -> None:
+        """
+        Will set the stop flag, and try and join the actor thread.
+        Has a 1s timeout by default.
+
+        :return: None
+        """
+        self._stop_flag = True
+        self.actor.join(timeout)
+
+    def force_stop(self) -> None:
+        """
+        The actor in this class is a Daemon, it is safe to just ignore it.
+        :return: None
+        """
+        self._stop_flag = True
+
+    def update_parameters(self, kill_streak: int, death_streak: int) -> None:
+        with self._param_lock:
+            self.ambient_vibration = max(
+                interpolate_value_bounded(
+                    float(kill_streak), 0.0, 15.0, 0.1, 0.6
+                ),
+                interpolate_value_bounded(
+                    float(death_streak), 0.0, 5.0, 0.1, 0.6
+                )
+            )
+            self.ambient_vibration_variance = max(
+                interpolate_value_bounded(
+                    float(kill_streak), 0.0, 15.0, 0.05, 0.33
+                ),
+                interpolate_value_bounded(
+                    float(death_streak), 0.0, 5.0, 0.05, 0.33
+                )
+            )
+            self.ambient_vibration_change_rate = min(
+                interpolate_value_bounded(
+                    float(kill_streak), 0.0, 15.0, 6.9, 2.5
+                ),
+                interpolate_value_bounded(
+                    float(death_streak), 0.0, 5.0, 6.9, 3.1
+                )
+            )
+            self.ambient_vibration_change_rate_variance = min(
+                interpolate_value_bounded(
+                    float(kill_streak), 0.0, 15.0, 0.5, 0.2
+                ),
+                interpolate_value_bounded(
+                    float(death_streak), 0.0, 5.0, 0.5, 0.3
+                )
+            )
+
+    def settle_ambience(self) -> None:
+        while not self._stop_flag:
+            time.sleep(0.05)
+            _now = time.time()
+            if _now - self.last_change_time > self.computed_change_rate:
+                if not self.intensity_controller.is_running():
+                    print("Intensity controller thread is not running, cannot control ambience.")
+                    break
+
+                with self._param_lock:
+                    _new_change_rate = uniform(
+                        max(0.33,
+                            self.ambient_vibration_change_rate - self.ambient_vibration_change_rate_variance
+                            ),
+                        self.ambient_vibration_change_rate + self.ambient_vibration_change_rate_variance
+                    )
+                    _new_ambient_intensity = uniform(
+                        max(0.0,
+                            self.ambient_vibration - self.ambient_vibration_variance,
+                            ),
+                        min(0.99,
+                            self.ambient_vibration + self.ambient_vibration_variance
+                            )
+                    )
+
+                self.computed_change_rate = _new_change_rate
+                self.last_change_time = _now
+                _slider = ValueSlider(
+                    self.intensity_controller.get_ambient_intensity(),
+                    _new_ambient_intensity,
+                    500.0,
+                    self.intensity_controller.set_ambient_intensity
+                )
+                self.intensity_controller.set_ambient_intensity_slider(_slider)
+        print("Exiting ambience control thread...")
+
 
 class Vibrator(metaclass=Singleton):
     client: Client = None
     connector: WebsocketConnector = None
     devices: list[Device] = None
     intensity_controller: IntensityController = None
+    ambient_intensity_controller: AmbienceController = None
     # Instantaneous vibrations
     current_vibration: float = None
-
-    # Ambient background vibration
-    last_change_time: float = None
-    computed_change_rate: float = None
-    _computed_ambient_vibration: float = None
-    ambient_vibration: float = None
-    ambient_vibration_variance: float = None
-    ambient_vibration_change_rate: float = None
-    ambient_vibration_change_rate_variance: float = None
 
     async def connect_and_scan(self) -> None:
         assert self.connector is not None
@@ -310,23 +436,14 @@ class Vibrator(metaclass=Singleton):
         self.client = Client("MAC Toys Client", ProtocolSpec.v3)
         self.connector = WebsocketConnector(f"ws://{ws_host}:{port}")
         self.intensity_controller = IntensityController(self.set_combined_intensity)
-        self.current_vibration = 0.0
-        self.ambient_vibration = 0.10
-        self.ambient_vibration_variance = 0.05
-        self.ambient_vibration_change_rate = 3.0
-        self.ambient_vibration_change_rate_variance = 0.5
-        self.computed_change_rate = self.ambient_vibration_change_rate
-        self.computed_ambient_vibration = self.ambient_vibration
-        self.last_change_time = time.time()
+        self.ambient_intensity_controller = AmbienceController(self.intensity_controller)
 
     def start(self) -> None:
+        rprint("[italics bright_black]Starting core controller...[/italics bright_black]")
         self.intensity_controller.start()
-
-    def get_computed_ambient_vibration(self) -> float:
-        return self.computed_ambient_vibration
-
-    def set_computed_ambient_vibration(self, value: float) -> None:
-        self.computed_ambient_vibration = min(max(value, 0.0), 1.0)
+        time.sleep(0.2)
+        rprint("[italics bright_black]Starting ambient controller...[/italics bright_black]")
+        self.ambient_intensity_controller.start()
 
     async def _apply_intensity(self) -> None:
         futures = []
@@ -357,36 +474,9 @@ class Vibrator(metaclass=Singleton):
 
         await self._apply_intensity()
 
-    def settle_ambience(self) -> None:
-        _now = time.time()
-        if _now - self.last_change_time > self.computed_change_rate:
-            _new_change_rate = uniform(
-                max(0.33,
-                    self.ambient_vibration_change_rate - self.ambient_vibration_change_rate_variance
-                    ),
-                self.ambient_vibration_change_rate + self.ambient_vibration_change_rate_variance
-            )
-            _new_ambient_intensity = uniform(
-                max(0.0,
-                    self.ambient_vibration - self.ambient_vibration_variance,
-                    ),
-                min(0.99,
-                    self.ambient_vibration + self.ambient_vibration_variance
-                    )
-            )
-            self.computed_change_rate = _new_change_rate
-            self.last_change_time = _now
-            _slider = ValueSlider(
-                self.intensity_controller.get_ambient_intensity(),
-                _new_ambient_intensity,
-                500.0,
-                self.intensity_controller.set_ambient_intensity
-            )
-            self.intensity_controller.set_ambient_intensity_slider(_slider)
-
-    def apply_instant_intensity(self, inital_intensity: float, duration: float) -> None:
+    def apply_instant_intensity(self, initial_intensity: float, duration: float) -> None:
         _slider = ValueSlider(
-            inital_intensity,
+            initial_intensity,
             0.0,
             duration,
             self.intensity_controller.set_instant_intensity
@@ -398,6 +488,7 @@ class Vibrator(metaclass=Singleton):
             await self.client.connect(self.connector)
 
     async def stop_all(self) -> None:
+        self.ambient_intensity_controller.stop()
         self.intensity_controller.stop()
         await self.client.disconnect()
 
@@ -418,9 +509,10 @@ def abort(signum, frame):
 
 
 def interaction_pane(output_queue: Queue):
-    while input("Type 'exit' at any time to exit program.").lower() != "exit":
+    while input("Type 'exit' at any time to exit program.\n").lower() != "exit":
         time.sleep(0.05)
 
+    rprint("[italic red]Exiting program...[/italic red]")
     output_queue.put(True)
 
 
@@ -439,7 +531,7 @@ async def main():
     _last_time = time.time() * 1000
     _time_elapsed: float = 0.0
 
-    print("Starting vibrator...")
+    rprint("[italic green]Starting vibrator...[/italic green]")
     _vibe.start()
     _flag_q: Queue = Queue()
     _stop_flag: bool = False
@@ -451,100 +543,69 @@ async def main():
     _thread.start()
 
     while not _stop_flag:
-        _vibe.settle_ambience()
         await _vibe.check_connection()
-        await _vibe.issue_command()
         if not _sse_listener.q_subscriber.empty():
             event = cast(Union[KillEvent, ChatEvent], _sse_listener.q_subscriber.get())
             _ks: Optional[int] = None
             _ds: Optional[int] = None
             _updates: list[UpdateTypes]
             if isinstance(event, ChatEvent):
-                # print(f"Chat: {event}")
                 _updates = _player_tracker.handle_chat_message(event)
                 if len(_updates) > 0:
-                    print(f"Notable Chat Event: {event}")
+                    rprint(f"[italic bright_black]Notable Chat Event: {event}[/italic bright_black]")
                 for update in _updates:
                     match update:
                         case UpdateTypes.CHAT_YOU_SAID_UWU:
-                            print("YOU SAID UWU/OWO -> GET VIBED")
+                            rprint("[bold bright_yellow]YOU SAID UWU/OWO -> GET VIBED[/bold bright_yellow]")
                             _vibe.apply_instant_intensity(0.4, 750.0)
                         case UpdateTypes.CHAT_FUCK_YOU:
-                            print("SOMEONES ANGRY -> MMM BZZZZZZ")
+                            rprint("[bold indian_red]SOMEONES ANGRY -> MMM BZZZZZZ[/bold indian_red]")
                             _vibe.apply_instant_intensity(0.3, 500.0)
 
             elif isinstance(event, KillEvent):
-                # print(f"Kill: {event}")
                 _ks, _ds, _updates = _player_tracker.add_kill_event(event)
-                _vibe.ambient_vibration = max(
-                    interpolate_value_bounded(
-                        float(_ks), 0.0, 15.0, 0.1, 0.6
-                    ),
-                    interpolate_value_bounded(
-                        float(_ds), 0.0, 5.0, 0.1, 0.6
-                    )
-                )
-                _vibe.ambient_vibration_variance = max(
-                    interpolate_value_bounded(
-                        float(_ks), 0.0, 15.0, 0.05, 0.33
-                    ),
-                    interpolate_value_bounded(
-                        float(_ds), 0.0, 5.0, 0.05, 0.33
-                    )
-                )
-                _vibe.ambient_vibration_change_rate = min(
-                    interpolate_value_bounded(
-                        float(_ks), 0.0, 15.0, 6.9, 2.5
-                    ),
-                    interpolate_value_bounded(
-                        float(_ds), 0.0, 5.0, 6.9, 3.1
-                    )
-                )
-                _vibe.ambient_vibration_change_rate_variance = min(
-                    interpolate_value_bounded(
-                        float(_ks), 0.0, 15.0, 0.5, 0.2
-                    ),
-                    interpolate_value_bounded(
-                        float(_ds), 0.0, 5.0, 0.5, 0.3
-                    )
-                )
+                _vibe.ambient_intensity_controller.update_parameters(_ks, _ds)
+
                 if len(_updates) > 0:
-                    print(f"{_player_tracker.player_name} kill streak: {_ks}, death streak: {_ds}")
+                    rprint(f"[italic bright_black]{_player_tracker.player_name} kill streak: {_ks}, "
+                           f"death streak: {_ds}[/italic bright_black]")
 
                 for update in _updates:
                     match update:
                         case UpdateTypes.GOT_KILLED:
-                            print("OH NO, YOU DIED -> *VIBRATES IN YOU*")
+                            rprint("[bold indian_red]OH NO, YOU DIED -> *VIBRATES IN YOU*[/bold indian_red]")
                             _vibe.apply_instant_intensity(0.4, 666.0)
                         case UpdateTypes.KILLED_ENEMY:
-                            print("GOOD GIRL/BOY/PUPPY/KITTY -> HAVE A REWARD")
+                            rprint("[bold green_yellow]GOOD GIRL/BOY/PUPPY/KITTY -> HAVE A REWARD[/bold green_yellow]")
                             _vibe.apply_instant_intensity(0.45, 750.0)
                         case UpdateTypes.CRIT_KILLED_ENEMY:
-                            print("FAIR AND BALANCED, BITCH! -> *GIBS YOU*")
+                            rprint("[bold aquamarine1]FAIR AND BALANCED, BITCH! -> *GIBS YOU*[/bold aquamarine1]")
                             _vibe.apply_instant_intensity(0.6, 850.0)
                         case UpdateTypes.GOT_CRIT_KILLED:
-                            print("LOL NOOB EZ -> *TOUCHES UR PROSTATE*")
+                            rprint("[bold dark_orange3]LOL NOOB EZ -> *TOUCHES UR PROSTATE*[/bold dark_orange3]")
                             _vibe.apply_instant_intensity(0.65, 1200.0)
 
+        await _vibe.issue_command()
         try:
             _value = _flag_q.get(block=False)
             if isinstance(_value, bool) and _value:
                 _stop_flag = _value
         except queue.Empty:
             pass
+
     _thread.join()
     _vibe = Vibrator()
-    print("Attempting stop of all vibrator components...")
+    rprint("[italic yellow]Attempting stop of all vibrator components...[/italic yellow]")
     asyncio.ensure_future(_vibe.stop_all(), loop=get_running_loop())
-    print("Killed vibrator component...")
+    rprint("[italic green]Killed vibrator component...[/italic green]")
 
-    print("Awaiting soft exit of SSEListener (will force exit after 2s)...")
+    rprint("[italic yellow]Awaiting soft exit of SSEListener (will force exit after 2s)...[/italic yellow]")
     _inst = SSEListener(event_endpoint=None)
     if _inst.t_subscriber is not None:
         _inst.shutdown_flag = True
         _inst.t_subscriber.join(timeout=2.0)
-    print("Killed SSE Listener...")
-    print("Vibe Controller Exiting...")
+    rprint("[italic green]Killed SSE Listener...[/italic green]")
+    rprint("[italic green]Vibe Controller Exiting...[/italic green]")
 
 
 if __name__ == "__main__":
